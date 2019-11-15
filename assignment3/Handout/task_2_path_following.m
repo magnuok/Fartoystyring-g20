1dfa7c87b8f95d2b8a73d4a756199b4f3a5c5dcd
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2019-11-15 15:46:48 +0100

    add task 2 part 2 starter file

---
+++ assignment3/Handout/task_2_path_following.m
@@ -0,0 +1,12 @@
+%% calculate crab angle
+% use equation 2.117 (assuming small angles)
+
+U = sqrt(v.^2 + u.^2);
+beta = v./U;
+
+
+
+%% plot
+% Show the problem by displaying a gure with the course ?, heading ?, desired course
+% ?d (from the guidance system) and crab angle ? from the simulation results acquired in Task 2.2.
+% Include the gure in the report and comment on the results.
