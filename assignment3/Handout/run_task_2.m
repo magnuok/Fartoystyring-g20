9ec7ba7f913009e3b71a97b241131796ba9757b9
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2019-11-15 15:07:55 +0100

    add run file for task 2

---
+++ assignment3/Handout/run_task_2.m
@@ -0,0 +1,37 @@
+dbstop if error
+load('WP.mat');
+%% constants
+tstart=0;           % Sim start time
+tstop=10000;        % Sim stop time
+tsamp=10;           % Sampling time for how often states are stored. (NOT ODE solver time step)
+                
+p0=zeros(2,1);      % Initial position (NED)
+v0=[3 0]';       % Initial velocity (body)
+psi0=0;             % Inital yaw angle
+r0=0;               % Inital yaw rate
+c=1;                % Current on (1)/off (0)
+
+dc = 0*180/pi;
+u_reference_0 = 3;
+u_reference_1 = 7;
+
+Kp_u = 50;
+Ki_u = 1e-2; %-1e-6;
+Kp = -8;
+Kd = -53;
+Ki = -1.6e-3;%-1e-6; 
+
+%% run simulation
+try 
+    sim('MSFartoystyring_task2');
+catch exception
+    error_var = lasterror;
+    stack_var = dbstack;
+    exception_var = getReport(exception, 'extended');
+end
+
+
+
+%% plot
+pathplotter(p(:,1), p(:,2), psi, tsamp, 4, tstart, tstop, 0, WP)
+
